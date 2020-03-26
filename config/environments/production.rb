8e6d87cce20f26d7c5031425c5b1491592eaf67e
Graham Lynch <grahamlynch10@hotmail.com>
2020-03-26 18:19:23 +0000

    production.rb file updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -53,7 +53,6 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.serve_static_files = true
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
