923712de6c9e3a292885694b3c80071c939d81c3
Graham Lynch <grahamlynch10@hotmail.com>
2020-03-27 08:43:05 +0000

    Initial Commit

--- Gemfile
+++ Gemfile
@@ -12,9 +12,7 @@ gem 'commentcheck', '~> 0.0.28'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 # Use sqlite3 as the database for Active Record
-group :development, :test do
-gem 'sqlite3'
-end
+
 
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
@@ -74,4 +72,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :production do
 gem 'pg'
 gem 'rails_12factor'
+end
+
+group :development, :test do
+gem 'sqlite3'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,17 +20,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  config.serve_static_files = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -55,7 +52,6 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
