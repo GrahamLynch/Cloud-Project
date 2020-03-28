c68925a6fc0e558f0f0c5b901e55c94aa4c881ce
Graham Lynch <grahamlynch10@hotmail.com>
2020-03-28 11:31:37 +0000

    modified profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,7 +11,7 @@ def show
 # Find an review in movies 1 that has id=2
 @profile = @user.profile
 @current = current_user
-@userproducts = Product.all.where("user_id %" + params[:user_id].to_i + "%")
+@userproducts = Product.all.where(user_id == params[:user_id])
 @user_decorator = helpers.decorate(current_user)
 @following = @user.active_relationships
 end
